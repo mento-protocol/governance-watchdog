85ca997a56e5dbc9a7236b30db1b0da0363305c5
Philip Paetz <philip.paetz@me.com>
2024-07-17 14:32:17 +0200

    chore: added DISCORD_WEBHOOK_URL to .env to enable local testing of cloud function
    previously, executions would fail because the local function lacked permissions to
    access secret manager and would throw an error. now, when NODE_ENV=development, it
    will try to load it from an env var instead

--- src/get-discord-webhook-url.ts
+++ src/get-discord-webhook-url.ts
@@ -3,8 +3,25 @@ import config from "./config.js";
 
 /**
  * Get the Discord webhook URL from GCloud Secret Manager
+ *
+ * NOTE: This will fail locally because the local function will lack the necessary permissions to access the secret.
+ * That's why read the webhook URL from our .env file when running locally. We could probably make it work by having
+ * the local function impersonate the service account used by the function in GCP, but that was a rabbit hole I didn't
+ * want to go down when a simple .env approach also works for local testing.
  */
 export default async function getDiscordWebhookUrl(): Promise<string> {
+  if (process.env.NODE_ENV === "development") {
+    const localWebhookUrl = config.DISCORD_WEBHOOK_URL;
+
+    if (!localWebhookUrl) {
+      throw new Error(
+        "Couldn't find DISCORD_WEBHOOK_URL in environment variables. Please set it in your .env file.",
+      );
+    }
+
+    return localWebhookUrl;
+  }
+
   const secretManager = new SecretManagerServiceClient();
   const [version] = await secretManager.accessSecretVersion({
     name: `projects/${config.GCP_PROJECT_ID}/secrets/${config.SECRET_NAME}/versions/latest`,
