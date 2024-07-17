5f560042c11b444ac56a781d1d280bf420147a4b
Philip Paetz <philip.paetz@me.com>
2024-07-17 15:39:12 +0200

    build: fine-tuned excluded files for function deployment via terraform

--- infra/storage.tf
+++ infra/storage.tf
@@ -22,7 +22,7 @@ data "archive_file" "function_source" {
   output_path = "${path.module}/../function-source.zip"
 
   # Not sure if this is stricly necessary when defining a .gcloudignore file, but better safe than sorry
-  excludes = [".env", ".env.yaml", ".git", ".gitignore", ".trunk", "*.sh", "*.md", "eslint.config.mjs", "infra", "node_modules"]
+  excludes = [".env", ".env.example", ".env.yaml", ".git", ".gitignore", ".trunk", ".vscode", "deploy-via-gcloud.sh", "test-deployed-function.sh", "README.md", "dist", "commitlint.config.mjs", "eslint.config.mjs", "infra", "node_modules"]
 }
 
 
