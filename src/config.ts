f02e8feec1dd60eaa5e9102d66cd0885ff389341
Philip Paetz <philip.paetz@me.com>
2024-07-17 14:32:17 +0200

    chore: added DISCORD_WEBHOOK_URL to .env to enable local testing of cloud function
    previously, executions would fail because the local function lacked permissions to
    access secret manager and would throw an error. now, when NODE_ENV=development, it
    will try to load it from an env var instead

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { JSONSchemaType, envSchema } from "env-schema";
 
 interface Env {
+  DISCORD_WEBHOOK_URL?: string;
   GCP_PROJECT_ID: string;
   SECRET_NAME: string;
 }
@@ -9,6 +10,7 @@ const schema: JSONSchemaType<Env> = {
   type: "object",
   required: ["GCP_PROJECT_ID", "SECRET_NAME"],
   properties: {
+    DISCORD_WEBHOOK_URL: { type: "string", nullable: true },
     GCP_PROJECT_ID: { type: "string" },
     SECRET_NAME: { type: "string" },
   },
