30e5dffbc5b070a256e8538b2c4b92eb25608fd6
Philip Paetz <philip.paetz@me.com>
2024-07-19 15:17:57 +0200

    chore: added convenience scripts to quickly access function logs

---
+++ get-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e          # fail on any error
+set -o pipefail # ensure non-zero exit codes are propagated in piped commands
+
+# Get the name of the watchdog-notifications function
+function_name=$(gcloud functions list --format="value(name)" | grep '^watchdog-notifications' | sed 's/.*//')
+
+# Fetch raw logs
+raw_logs=gcloud functions logs read "${function_name}" \
+	--region europe-west1 \
+	--format json \
+	--sort-by TIME_UTC
+
+# Format logs
+echo "${raw_logs}" | jq -r '.[] | if .level == "E" then 
+  "\u001b[31m[\(.level)]\u001b[0m \u001b[33m\(.time_utc)\u001b[0m: \(.log)" 
+else 
+  "[\(.level)] \u001b[33m\(.time_utc)\u001b[0m: \(.log)" 
+end'
