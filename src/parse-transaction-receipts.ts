2353b4d8f783c9ab4af104e21e2dda5c04d42a2e
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2024-07-29 14:45:56 +0200

    refactor: better error handling when parsing events (#8)

--- src/parse-transaction-receipts.ts
+++ src/parse-transaction-receipts.ts
@@ -1,10 +1,13 @@
+import assert from "assert/strict";
+
 // External
 import { decodeEventLog } from "viem";
 
 // Internal
 import GovernorABI from "./governor-abi.js";
-import { HealthCheckEvent, ProposalCreatedEvent } from "./types.js";
+import { EventType, HealthCheckEvent, ProposalCreatedEvent } from "./types.js";
 import hasLogs from "./utils/has-logs.js";
+import getEventByTopic from "./utils/get-event-by-topic.js";
 import isHealthCheckEvent from "./utils/is-health-check-event.js";
 import isProposalCreatedEvent from "./utils/is-proposal-created-event.js";
 import isTransactionReceipt from "./utils/is-transaction-receipt.js";
@@ -45,48 +48,59 @@ export default function parseTransactionReceipts(
     }
 
     for (const log of receipt.logs) {
-      if (!log.topics) {
-        throw new Error("No topics found in log");
+      assert(log.topics && log.topics.length > 0, "No topics found in log");
+
+      const eventSignature = log.topics[0];
+      const eventType = getEventByTopic(eventSignature);
+
+      switch (eventType) {
+        case EventType.Unknown:
+          // It can happen that a single transaction fires multiple events,
+          // some of which we are not interested in
+          continue;
+        case EventType.ProposalCreated: {
+          const event = decodeEventLog({
+            abi: GovernorABI,
+            data: log.data as `0x${string}`,
+            topics: log.topics as [
+              signature: `0x${string}`,
+              ...args: `0x${string}`[],
+            ],
+          });
+
+          if (isProposalCreatedEvent(event)) {
+            result.push({
+              event,
+              txHash: log.transactionHash,
+            });
+          }
+          break;
+        }
+        case EventType.MedianUpdated: {
+          const event = decodeEventLog({
+            abi: SortedOraclesABI,
+            data: log.data as `0x${string}`,
+            topics: log.topics as [
+              signature: `0x${string}`,
+              ...args: `0x${string}`[],
+            ],
+          });
+
+          if (isHealthCheckEvent(event)) {
+            result.push({
+              block: Number(receipt.blockNumber),
+              event,
+              txHash: log.transactionHash,
+            });
+          }
+          break;
+        }
+        default:
+          assert(
+            false,
+            `Unknown event type. Did you forget to add a new event?`,
+          );
       }
-
-      try {
-        const event = decodeEventLog({
-          abi: GovernorABI,
-          data: log.data as `0x${string}`,
-          topics: log.topics as [
-            signature: `0x${string}`,
-            ...args: `0x${string}`[],
-          ],
-        });
-
-        if (isProposalCreatedEvent(event)) {
-          result.push({
-            event,
-            txHash: log.transactionHash,
-          });
-        }
-        // eslint-disable-next-line no-empty
-      } catch {}
-
-      try {
-        const event = decodeEventLog({
-          abi: SortedOraclesABI,
-          data: log.data as `0x${string}`,
-          topics: log.topics as [
-            signature: `0x${string}`,
-            ...args: `0x${string}`[],
-          ],
-        });
-
-        if (isHealthCheckEvent(event)) {
-          result.push({
-            block: Number(receipt.blockNumber),
-            event,
-            txHash: log.transactionHash,
-          });
-        }
-        // eslint-disable-next-line no-empty
-      } catch {}
     }
   }
 
