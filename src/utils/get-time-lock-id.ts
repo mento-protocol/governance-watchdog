929b275cac7fe76c4bbc3efd9f1565809d6242f2
Philip Paetz <philip.paetz@me.com>
2024-08-06 10:56:53 +0200

    chore: add timelock operation id explainer

--- src/utils/get-time-lock-id.ts
+++ src/utils/get-time-lock-id.ts
@@ -1,7 +1,16 @@
-import { encodeAbiParameters, parseAbiParameters, keccak256 } from "viem";
+import { encodeAbiParameters, keccak256, parseAbiParameters } from "viem";
 
 import { ProposalCreatedEvent } from "../types";
 
+/**
+ * Given a ProposalCreatedEvent, calculate the corresponding timelock operation ID.
+ * Governance Watchdogs need the timelock operation ID to veto queued proposals.
+ *
+ * The governor proposal ID and the timelock operation ID are not the same, which can
+ * be confusing. They use different hashing mechanisms to calculate their respective IDs:
+ * - Timelock Controller Operation IDs: https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/58fa0f81c4036f1a3b616fdffad2fd27e5d5ce21/contracts/governance/TimelockControllerUpgradeable.sol#L218
+ * - Governor Proposal IDs: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/0a25c1940ca220686588c4af3ec526f725fe2582/contracts/governance/Governor.sol#L139
+ */
 export default function getProposalTimeLockId(
   event: ProposalCreatedEvent,
 ): string {
